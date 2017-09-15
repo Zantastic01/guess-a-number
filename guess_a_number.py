03b5332e3266af77b98c1cdef7bfe72660003e2d
Zantastic01 <31477547+Zantastic01@users.noreply.github.com>
2017-09-15 09:18:59 -0400

    Added input validation

--- guess_a_number.py
+++ guess_a_number.py
@@ -1,13 +1,31 @@
 import random
 
-rand = random.randint(1, 100)
-print("I'm thinking of a number from 1 to 100.");
+#config
+low = 1
+high = 100
+limit = 10
+
+# start game
+rand = random.randint(low, high)
+print("I'm thinking of a number from " + str(low) + " to " + str(high))
 
 guess = -1
+tries = 0
 
-while guess != rand:
-    guess = input("Take a guess: ")
-    guess = int(guess)
+#helper functions
+def get_guess():
+        while True:
+            g = input("Take a Guess: ")
+
+            if g.isnumeric():
+                g = int(g)
+                return g
+            else:
+                print("You must enter a number")
+                
+# play the game
+while guess != rand and tries < limit:
+    guess = get_guess()
     
     if guess < rand:
         print("You guessed too low.")
@@ -16,4 +34,11 @@ while guess != rand:
     else:
         print("You got it!")
 
-print("Game over")
+        tries += 1
+
+# tell player outcome
+if guess == rand:
+    print("You win.")
+else:
+    print("You suck, i was thinking of "  + str(rand) + ".")
+    
