64e261e37368688043a27b8794da89cf1d6f5e6c
Jones <zjones0935@greenville.k12.sc.us>
2017-09-25 09:19:57 -0400

    updated numbers

--- guess_a_number.py
+++ guess_a_number.py
@@ -1,44 +1,101 @@
 import random
+import math
 
-#config
+# config
 low = 1
 high = 100
-limit = 10
+limit = math.ceil(math.log(high - low +1,2))
 
-# start game
-rand = random.randint(low, high)
-print("I'm thinking of a number from " + str(low) + " to " + str(high))
+# helper functions
+def show_start_screen():
+    print("      _,---.                  ,----.    ,-,--.    ,-,--.           ,---.             .-._                           ___                  ,----.              ")
+    print("  _.='.'-,  \ .--.-. .-.-. ,-.--` , \ ,-.'-  _\ ,-.'-  _\        .--.'  \           /==/ \  .-._ .--.-. .-.-..-._ .'=.'\    _..---.   ,-.--` , \  .-.,.---.  ")             
+    print(" /==.'-     //==/ -|/=/  ||==|-  _.-`/==/_ ,_.'/==/_ ,_.'        \==\-/\ \          |==|, \/ /, /==/ -|/=/  /==/ \|==|  | .' .'.-. \ |==|-  _.-` /==/  `   \ ")             
+    print("/==/ -   .-' |==| ,||=| -||==|   `.-.\==\  \   \==\  \           /==/-|_\ |         |==|-  \|  ||==| ,||=| -|==|,|  / - |/==/- '=' / |==|   `.-.|==|-, .=., | ")            
+    print("|==|_   /_,-.|==|- | =/  /==/_ ,    / \==\ -\   \==\ -\          \==\,   - \        |==| ,  | -||==|- | =/  |==|  \/  , ||==|-,   ' /==/_ ,    /|==|   '='  / ")            
+    print("|==|  , \_.' )==|,  \/ - |==|    .-'  _\==\ ,\  _\==\ ,\         /==/ -   ,|        |==| -   _ ||==|,  \/ - |==|- ,   _ ||==|  .=. \|==|    .-' |==|- ,   .'  ")            
+    print("\==\-  ,    (|==|-   ,   /==|_  ,`-._/==/\/ _ |/==/\/ _ |       /==/-  /\ - \       |==|  /\ , ||==|-   ,   /==| _ /\   |/==/- '=' ,|==|_  ,`-._|==|_  . ,'.  ")            
+    print(" /==/ _  ,  //==/ , _  .'/==/ ,     /\==\ - , /\==\ - , /       \==\ _.\=\.-'       /==/, | |- |/==/ , _  .'/==/  / / , /==|   -   //==/ ,     //==/  /\ ,  ) ")            
+    print(" `--`------' `--`..---'  `--`-----``  `--`---'  `--`---'         `--`               `--`./  `--``--`..---'  `--`./  `--``-._`.___,' `--`-----`` `--`-`--`--'  ")                                 
 
-guess = -1
-tries = 0
-
-#helper functions
-def get_guess():
-        while True:
-            g = input("Take a Guess: ")
-
-            if g.isnumeric():
-                g = int(g)
-                return g
-            else:
-                print("You must enter a number")
-                
-# play the game
-while guess != rand and tries < limit:
-    guess = get_guess()
+def show_credits():
+    print("This awesome game was created by $$$$$$$$\  $$$$$$\  $$\   $$\       $$\      $$\  $$$$$$\  $$\   $$\ ")
+    print("                                 \____$$  |$$  __$$\ $$$\  $$ |      $$$\    $$$ |$$  __$$\ $$$\  $$ |")
+    print("                                     $$  / $$ /  $$ |$$$$\ $$ |      $$$$\  $$$$ |$$ /  $$ |$$$$\ $$ |")
+    print("                                    $$  /  $$$$$$$$ |$$ $$\$$ |      $$\$$\$$ $$ |$$$$$$$$ |$$ $$\$$ |")
+    print("                                   $$  /   $$  __$$ |$$ \$$$$ |      $$ \$$$  $$ |$$  __$$ |$$ \$$$$ |")
+    print("                                  $$  /    $$ |  $$ |$$ |\$$$ |      $$ |\$  /$$ |$$ |  $$ |$$ |\$$$ |")
+    print("                                 $$$$$$$$\ $$ |  $$ |$$ | \$$ |      $$ | \_/ $$ |$$ |  $$ |$$ | \$$ |")
+    print("                                 \________|\__|  \__|\__|  \__|      \__|     \__|\__|  \__|\__|  \__|")
+                                                                     
+                                                                     
+                                                                  
     
+def get_guess():
+    while True:
+        guess = input("Guess a number: ")
+
+        if guess.isnumeric():
+            guess = int(guess)
+            return guess
+        else:
+            print("You must enter a number.")
+
+def pick_number():
+    print("I'm thinking of a number from " + str(low) + " to " + str(high) +".")
+    print ("You get " + str(limit) + " tries.")
+
+
+    return random.randint(low, high)
+
+def check_guess(guess, rand):
     if guess < rand:
         print("You guessed too low.")
     elif guess > rand:
         print("You guessed too high.")
+
+def show_result(guess, rand):
+    if guess == rand:
+        print("You win!")
     else:
-        print("You got it!")
+        print("You are such a loser! The number was " + str(rand) + ".")
+
+def play_again():
+    while True:
+        decision = input("Would you like to play again? (y/n) ")
+
+        if decision == 'y' or decision == 'yes':
+            return True
+        elif decision == 'n' or decision == 'no':
+            return False
+        else:
+            print("I don't understand. Please enter 'y' or 'n'.")
+
+def play():
+    guess = -1
+    tries = 0
+
+    rand = pick_number()
+    
+    while guess != rand and tries < limit:
+        guess = get_guess()
+        check_guess(guess, rand)
 
         tries += 1
 
-# tell player outcome
-if guess == rand:
-    print("You win.")
-else:
-    print("You suck, i was thinking of "  + str(rand) + ".")
-    
+    show_result(guess, rand)
+
+
+# Game starts running here
+show_start_screen()
+
+playing = True
+
+while playing:
+    play()
+    playing = play_again()
+
+show_credits()
+
+
+
